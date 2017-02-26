1a166e027e2564d7e3f252f431f9a8ce30b52921
James <dyenamite@hotmail.co.uk>
2017-02-26 20:03:39 +1100

    working version

--- assignment1/assignment1-starter-code/app.js
+++ assignment1/assignment1-starter-code/app.js
@@ -1,5 +1,27 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('LunchCheck', [])
-};
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+      LunchCheckController.$inject = ['$scope'];
+          function LunchCheckController($scope) {
+
+                $scope.ShowMessage = function () {
+                  var items = $scope.list;
+                  console.log(items);
+
+                  if (items == undefined || items == "") {
+				                 $scope.message = "Please enter data first";
+			                    } else {
+				                         var list = items.split(",")
+				                             if (list.length > 3) {
+					                                  $scope.message = "Too much!";
+				                                      } else {
+					                                           $scope.message = "Enjoy!";
+                                                   };
+                                                 };
+                                                 $scope.list = undefined
+                                               };
+  }
+})();
